e7da2098c60e23063dacb2ca8bc475c555ece781
djl <yuuyoo@163.com>
2020-02-29 17:25:10 +0800

    增加读书列表springmvc控制器

---
+++ src/main/java/cn/technotes/readinglist/ReadingListController.java
@@ -0,0 +1,58 @@
+package cn.technotes.readinglist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * 功能：读书列表控制
+ *
+ * @author：DJL
+ * @create：2020/2/29 17:08
+ * @version：2020 Version：1.0
+ */
+@Controller
+@RequestMapping("/")
+public class ReadingListController {
+
+    private ReadingListRepository readingListRepository;
+
+    @Autowired
+    public ReadingListController(ReadingListRepository readingListRepository) {
+        this.readingListRepository = readingListRepository;
+    }
+
+    /**
+     * 根据读者获取读书列表
+     * @param reader
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/{reader}", method = RequestMethod.GET)
+    public String readersBooks(@PathVariable("reader") String reader, Model model) {
+        List<Book> readingList = readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            model.addAttribute("books", readingList);
+        }
+
+        return "readingList";
+    }
+
+    /**
+     * 添加读书列表
+     * @param reader
+     * @param book
+     * @return
+     */
+    @RequestMapping(value = "/{reader}", method = RequestMethod.POST)
+    public String addToReadingList(@PathVariable("reader") String reader, Book book) {
+        book.setReader(reader);
+        readingListRepository.save(book);
+        return "redirect:/{reader}";
+    }
+}

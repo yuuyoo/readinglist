8ca1c055a83dda870ba54db21185da80c65604c8
djl <yuuyoo@163.com>
2020-03-03 14:26:47 +0800

    修改项目打成war包部署

--- src/main/java/cn/technotes/readinglist/ReadingListApplication.java
+++ src/main/java/cn/technotes/readinglist/ReadingListApplication.java
@@ -2,12 +2,18 @@ package cn.technotes.readinglist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ReadingListApplication {
+public class ReadingListApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ReadingListApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ReadingListApplication.class);
+    }
 }
